d5ec3772edccc57d66df5038c094d8d6f22529d6
o06660o <2957531723@qq.com>
2025-09-22 16:45:48 +0800

    disable ", ', ` from nvim-autopairs

--- lua/plugin/nvim-autopairs.lua
+++ lua/plugin/nvim-autopairs.lua
@@ -1,5 +1,12 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  config = true,
+  config = function()
+    local npairs = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+    npairs.setup({})
+    npairs.remove_rule('"')
+    npairs.remove_rule("'")
+    npairs.remove_rule("`")
+  end,
 }
