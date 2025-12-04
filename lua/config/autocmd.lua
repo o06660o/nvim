f8b24ae0c560eca9255303ef01fa1a12b113787d
o06660o <2957531723@qq.com>
2025-12-04 09:03:30 +0800

    add markdown autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -43,3 +43,30 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
     vim.opt_local.filetype = "menhir"
   end,
 })
+
+-- Convert markdown to pdf
+vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+  pattern = "markdown",
+  callback = function(args)
+    local bufnr = args.buf
+    if vim.b[bufnr].markdown_keymap_set then
+      return
+    end
+    vim.b[bufnr].markdown_keymap_set = true
+
+    vim.api.nvim_buf_set_keymap(
+      bufnr,
+      "n",
+      "<C-A-N>",
+      ":wa<CR>:!pandoc '%' -o '%:r.pdf' --pdf-engine=xelatex -V fontsize=12pt -V geometry:left=0.6in,right=0.6in,top=0.7in,bottom=0.7in -V monofont='FiraMono Nerd Font' -V italicfont='*' -V CJKmainfont='Sarasa Term SC Nerd' -V CJKmonofont='Sarasa Term SC Nerd' -V fontset=none <CR>",
+      { noremap = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      bufnr,
+      "n",
+      "<F5>",
+      ":wa<CR>:silent !zathura '%:r.pdf' >/dev/null 2>&1 &<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
+})
